6593a99c08775ad96da3af48d3916abebe6eb210
LienVNH <lien.vannieuwenhuyse1@student.arteveldehs.be>
2023-04-24 15:00:10 +0200

    commit opdracht2/index.html

---
+++ opdracht2/js/scroll.js
@@ -0,0 +1,16 @@
+let animals = gsap.utils.toArray('article');
+
+animals.forEach(animal => {
+
+gsap.from(animal, {
+    scrollTrigger:{
+        trigger:article,
+        toggleActions: "play reverse play reverse",
+        start: "top 80%",
+        end: "bottom 20%",
+    },
+    duration: 0.5, 
+    x: -300, 
+    opacity: 0});
+
+});
