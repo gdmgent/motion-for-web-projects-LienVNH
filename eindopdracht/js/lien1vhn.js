0d2c0ce085294bd1020db7faa2862f8ce8f3e3ad
LienVNH <lien.vannieuwenhuyse1@student.arteveldehs.be>
2023-05-23 11:10:32 +0200

    eindopdracht aangepast

--- eindopdracht/js/lien1vhn.js
+++ eindopdracht/js/lien1vhn.js
@@ -2,21 +2,30 @@ function lien1vhn (fase) {
     console.log("lien1vhn.js: fase=" + fase);
     let section = document.getElementById("lien1vhn");
     
+    
     if(!section.init){
         section.init = true;
         // eigen animatie schrijven
         console.log("lien1vhn.js: init");
         section.tl
-        .to("#lien1vhn .box",{
-            rotation: 360,
-            left:0,
-            top:0,
-            width:"100%",
-            height:"100%",
-        })
+        .from("#lien1vhn .square-white",{
             
-        }
-    }
+            width:"100vw",
+            height:"100vw",
+            skewY: "45",
+        })
+
+        .to("#lien1vhn .box",{
+            rotateX: "60",
+            rotateZ: "115",
+            rotateY: "115",
+            
+            
+            })
+
+       
+      
+    }}
         
             
     
