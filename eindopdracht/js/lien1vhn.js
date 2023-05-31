cf9e8b5160f556c8e4f78be2dc6bfc779a1f2ce7
LienVNH <lien.vannieuwenhuyse1@student.arteveldehs.be>
2023-05-31 06:37:28 +0200

    correct index,adding images and  styling

--- eindopdracht/js/lien1vhn.js
+++ eindopdracht/js/lien1vhn.js
@@ -1,32 +1,35 @@
-function lien1vhn (fase) {
-    console.log("lien1vhn.js: fase=" + fase);
-    let section = document.getElementById("lien1vhn");
-    
-    
-    if(!section.init){
-        section.init = true;
-        // eigen animatie schrijven
-        console.log("lien1vhn.js: init");
-        section.tl
-        .from("#lien1vhn .square-white",{
-            
-            width:"100vw",
-            height:"100vw",
-            skewY: "45",
-        })
+function lien1vhn(fase) {
+  console.log("lien1vhn.js: fase=" + fase);
+  let section = document.getElementById("lien1vhn");
 
-        .to("#lien1vhn .box",{
-            rotateX: "60",
-            rotateZ: "115",
-            rotateY: "115",
-            
-            
-            })
+  if (!section.init) {
+    section.init = true;
+    // eigen animatie schrijven
+    console.log("lien1vhn.js: init");
+    section.tl
+      .from("#lien1vhn .square-white", {
+        width: "20vw",
+        height: "20vw",
+      })
 
-       
+      .to("#lien1vhn .box", {
+        rotateX: "50",
+        rotateZ: "210",
+        rotateY: "200",
+        scale: "2",
+        transition: "preserve-3d",
+        border: "4px solid darkgrey",
+      })
+    
+      .add(".intro" ,{
+      progress: (0.5),})
       
-    }}
-        
-            
-    
+     
+   
+   
+          
+}
+}     
+      
+
 
