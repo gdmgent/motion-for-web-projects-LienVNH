00e51809ad616d31416c3ff8559ef36aa483c493
Dieter De Weirdt <dieter.deweirdt@arteveldehs.be>
2023-05-31 10:43:03 +0200

    Fix timeline

--- eindopdracht/js/lien1vhn.js
+++ eindopdracht/js/lien1vhn.js
@@ -7,7 +7,7 @@ function lien1vhn(fase) {
     // eigen animatie schrijven
     console.log("lien1vhn.js: init");
     section.tl
-      .add(".intro")
+      .add("intro")
         .from("#lien1vhn .square-white", {
           width: "20vw",
           height: "20vw",
@@ -20,6 +20,43 @@ function lien1vhn(fase) {
           transition: "preserve-3d",
           border: "4px solid darkgrey",
         }, "intro")
+        .add("king")
+          .to("#lien1vhn .box", {
+            scale: "2",
+          }, "king")
+          .to("#lien1vhn .kinggold", {
+            left: "50%",
+            top: "50%",
+            width: "10vw",
+            height: "10vh",
+            objectFit: "contain",
+            position: "fixed",
+          } , "king")
+        .add("queen")
+          .to("#lien1vhn .box", {
+            scale: "2",
+          }, "queen")
+
+        .add("asdf")
+
+
+          
+        .add("qwer")
+        .add("terw")
+        .add("rtuy")
+      .add("reset")
+        .from("#lien1vhn .square-black", {
+          //opacity: 0,
+        }, "reset")
+        .to("#lien1vhn .box", {
+          rotateX: "0",
+          rotateZ: "0",
+          rotateY: "0",
+          scale: "1",
+          transition: "preserve-3d",
+          border: "0px solid darkgrey",
+        }, "reset")
+        
           
   }
 }     
