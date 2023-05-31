9f77507a614224f6a87f4d4f461a0b90a1ad4521
Dieter De Weirdt <dieter.deweirdt@arteveldehs.be>
2023-05-31 10:28:21 +0200

    add gsap + add timeline intro

--- eindopdracht/js/lien1vhn.js
+++ eindopdracht/js/lien1vhn.js
@@ -7,28 +7,21 @@ function lien1vhn(fase) {
     // eigen animatie schrijven
     console.log("lien1vhn.js: init");
     section.tl
-      .from("#lien1vhn .square-white", {
-        width: "20vw",
-        height: "20vw",
-      })
-
-      .to("#lien1vhn .box", {
-        rotateX: "50",
-        rotateZ: "210",
-        rotateY: "200",
-        scale: "2",
-        transition: "preserve-3d",
-        border: "4px solid darkgrey",
-      })
-    
-      .add(".intro" ,{
-      progress: (0.5),})
-      
-     
-   
-   
+      .add(".intro")
+        .from("#lien1vhn .square-white", {
+          width: "20vw",
+          height: "20vw",
+        }, "intro")
+        .to("#lien1vhn .box", {
+          rotateX: "50",
+          rotateZ: "210",
+          rotateY: "200",
+          scale: "2",
+          transition: "preserve-3d",
+          border: "4px solid darkgrey",
+        }, "intro")
           
-}
+  }
 }     
       
 
